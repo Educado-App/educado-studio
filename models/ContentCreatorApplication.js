33666c68ef56ce507d316015f4af75071469eccc
Abdallah Ziad Al Naif <Abdallahalnaif@hotmail.com>
2022-10-13 12:31:05 +0200

    Updated so now we have a get request
    Get request for the backend to retrieve all the ContentCreator Applicant information.

--- models/ContentCreatorApplication.js
+++ models/ContentCreatorApplication.js
@@ -3,19 +3,17 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 // Class description
-const ContentCreatorSchema = new Schema({
+const ContentCreator = new Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
   email: { type: String, required: true },
   motivationTextBox: { type: String },
   createdAt: { type: Date, default: new Date() },
-  approved: { type: Boolean, default: false },
-  rejectionReason: {type: String, required: false}
 });
 
 const ContentCreatorApplication = mongoose.model(
   "Content-Creator-Application",
-  ContentCreatorSchema
+  ContentCreator
 );
 
 module.exports = { ContentCreatorApplication };
--- routes/signupRoutes.js
+++ routes/signupRoutes.js
@@ -1,12 +1,12 @@
+const { app } = require("cli");
 const {
   ContentCreatorApplication,
 } = require("../models/ContentCreatorApplication");
 
-
 const router = require("express").Router();
 
 // Content Creator Application Route
-router.post("/content-creator", async (req, res) => {
+router.post("/signup/content-creator", async (req, res) => {
   const form = req.body;
 
   // Validate form ...
@@ -16,6 +16,19 @@ router.post("/content-creator", async (req, res) => {
 
     res.status(201);
     res.send(created);
+  } catch (error) {
+    res.status(400);
+    res.send(error.message);
+  }
+});
+
+// Content Creator Application Route
+router.get("/signup/content-creator/GetAllApplications", async (req, res) => {
+  try {
+    const listOfForm = await ContentCreatorApplication.find();
+    res.json(listOfForm);
+
+    res.status(201);
   } catch (error) {
     res.status(400);
     res.send(error.message);
