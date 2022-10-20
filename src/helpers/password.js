a1e71ba28d3203eab6a6ca108034f1365c94d954
Hans Heje <hansheje@hotmail.com>
2022-10-20 14:17:45 +0200

    feat: added random password generation

--- src/helpers/password.js
+++ src/helpers/password.js
@@ -4,7 +4,8 @@ const SALT_ROUNDS = 1000
 
 module.exports = Object.freeze({
     encrypt,
-    isValid
+    isValid,
+    generateRandomPassword
 })
 
 function encrypt(plainPassword) {
@@ -17,4 +18,16 @@ function encrypt(plainPassword) {
 function isValid({ password, hash, salt }) {
     let hashVerify = crypto.pbkdf2Sync(password, salt, SALT_ROUNDS, 64, 'sha256').toString('hex');
     return hash === hashVerify;
+}
+
+function generateRandomPassword() {
+    var chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var passwordLength = 12;
+    var password = "";
+    for (var i = 0; i <= passwordLength; i++) {
+        var randomNumber = Math.floor(Math.random() * chars.length);
+        password += chars.substring(randomNumber, randomNumber + 1);
+    }
+    return password;
+
 }
