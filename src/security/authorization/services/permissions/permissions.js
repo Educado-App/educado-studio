a02bdc4071bbba1137f498a07592d6eda0a34dd1
Hans Heje <hansheje@hotmail.com>
2022-11-25 12:10:04 +0100

    added permissions

--- src/security/authorization/services/permissions/permissions.js
+++ src/security/authorization/services/permissions/permissions.js
@@ -6,12 +6,15 @@ module.exports = Permissions = {
 	'EDIT_INSTITUTION' : "Edit institution",
 	'ASSIGN_MODERATORS' : "Assign moderators",
 	'DEASSIGN_MODERATORS' : "Remove moderators",
-	'REMOVE_MEMBER' : "Remove member",
 	'ADD_MEMBER': "Add member",
+	'REMOVE_MEMBER' : "Remove member",
+	'ADD_COURSE' : "Add course",
+	'VIEW_COURSE' : "View course",
+	'EDIT_COURSE' : "Edit course",
+	'HANDLE_CC_APPLICATIONS' : "Update motivations",
 	'HANDLE_CC_APPLICATIONS' : "Update content creator applictions",
 	'HANDLE_INSTITUTION_APPLICATIONS' : "Update institution applications",
 	'HANDLE_PUBLISH_COURSE': "Updates publish course",
-	'HANDLE_CC_APPLICATIONS' : "Update motivations",
 };
 
 
