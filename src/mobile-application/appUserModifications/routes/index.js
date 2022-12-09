705a001c086cc4566cdd6ea6472f947f669c23b0
Cthaar <cthaar20@student.aau.dk>
2022-12-09 16:58:18 +0100

    Refactored

--- src/mobile-application/appUserModifications/routes/index.js
+++ src/mobile-application/appUserModifications/routes/index.js
@@ -14,12 +14,10 @@ const { registerAppUserController, deleteAppUserController } = require('../contr
 // This can be used to restrict any routes
 // User needs to be logged in
 const { restrictedApp } = require('../../appSecurity')
-const { appUserModel } = require('../data-access/appUserDb')
+
 const { CourseModel } = require('../../../courses/db-models/Course')
 const { SectionModel } = require('../../../courses/db-models/Section')
-const { courseList } = require('../../../courses/gateways/courseList')
-const { sectionList } = require('../../../courses/gateways/sectionList')
-const { exerciseList } = require('../../../courses/gateways/exerciseList')
+const { appUserList } = require('../gateways')
 
 
 
@@ -28,7 +26,7 @@ router.delete("/delete/:id",  makeExpressCallback(deleteAppUserController))
 
 router.put("/:id/updateCourse/:courseId", async function(req, res, next) {
   try {
-    const user = await appUserModel.findById(req.params.id);
+    const user = await appUserList.findById(req.params.id);
     if (!user) {
       return res.status(404).json({
         message: 'User not found'
@@ -63,7 +61,7 @@ router.put("/:id/updateCourse/:courseId", async function(req, res, next) {
 
 router.put("/:id/updateSection/:sectionId", async function(req, res, next) {
   try {
-    const user = await appUserModel.findById(req.params.id);
+    const user = await appUserList.findById(req.params.id);
     if (!user) {
       return res.status(404).json({
         message: 'User not found'
@@ -71,21 +69,14 @@ router.put("/:id/updateSection/:sectionId", async function(req, res, next) {
     }
 
     // find the course in the user's activeCourses list
-    let course;
-    for (const userCourse of user.activeCourses) {
-      if (userCourse.courseId == req.params.courseId) {
-        course = userCourse; // store a reference to the existing course object
-      }
-    }
-
-    // Error if course does not exists
+    const course = await findCourseById(user, req.params.courseId);
     if (!course) {
       return res.status(404).json({
         message: 'Course not found'
       });
     }
 
-    // set the isComplete property of the course object to true
+    // update isComplete of the course with true
     course.isComplete = true;
 
     await user.save();
@@ -99,7 +90,7 @@ router.put("/:id/updateSection/:sectionId", async function(req, res, next) {
 router.put("/:id/updateSection/:courseId/:sectionId", async function(req, res, next) {
   try {
     // find the user by ID
-    const user = await appUserModel.findById(req.params.id);
+    const user = await appUserList.findById(req.params.id);
     if (!user) {
       return res.status(404).json({
         message: 'User not found'
@@ -107,54 +98,36 @@ router.put("/:id/updateSection/:courseId/:sectionId", async function(req, res, n
     }
 
     // find the course in the user's activeCourses list
-    let course;
-    for (const userCourse of user.activeCourses) {
-      // check if the courseId of the existing course matches the courseId of the course that the user is trying to update
-      if (userCourse.courseId == req.params.courseId) {
-        course = userCourse; // store a reference to the existing course object
-      }
-    }
-
-    // if the course does not exist in the user's activeCourses list, return an error message
+    const course = await findCourseById(user, req.params.courseId);
     if (!course) {
       return res.status(404).json({
         message: 'Course not found'
       });
     }
 
-// find the section in the course's sections array
-let section;
-for (const courseSection of course.sections) {
-  // check if the sectionId of the existing section matches the sectionId of the section that the user is trying to update
-  if (courseSection.section == req.params.sectionId) { // use the correct property name here
-    section = courseSection; // store a reference to the existing section object
-  }
-}
+    // find the section in the course's sections array
+    const section = await findSectionById(course, req.params.sectionId);
+    if (!section) {
+      return res.status(404).json({
+        message: 'Section not found'
+      });
+    }
 
-// if the section does not exist in the course's sections array, return an error message
-if (!section) {
-  return res.status(404).json({
-    message: 'Section not found'
-  });
-}
-
-    // update the isComplete property of the section object with the value provided by the frontend
-    section.isComplete = true; // set the isComplete property to true
-
-    // save the updated user object to the database
+    // update isComplete of the section with true
+    section.isComplete = true; 
     await user.save();
 
-    // return the updated user object in the response
     res.json(user);
   } catch (err) {
     return next(err);
   }
 });
 
+
+// If user completes an exercise it will update its isComplete status in the user document
 router.put("/:id/updateExercise/:courseId/:sectionId/:exerciseId", async function(req, res, next) {
   try {
-    // find the user by ID
-    const user = await appUserModel.findById(req.params.id);
+    const user = await appUserList.findById(req.params.id);
     if (!user) {
       return res.status(404).json({
         message: 'User not found'
@@ -185,23 +158,26 @@ router.put("/:id/updateExercise/:courseId/:sectionId/:exerciseId", async functio
       });
     }
 
-    // update the isComplete property of the exercise object with the value provided by the frontend
-    exercise.isComplete = true; // set the isComplete property to true
-
-    // save the updated user object to the database
+    // update isComplete of the exercise with true
+    exercise.isComplete = true;
     await user.save();
 
-    // return the updated user object in the response
     res.json(user);
   } catch (err) {
     return next(err);
   }
 });
 
-router.post('/:id/enroll/:courseId', async function(req, res, next) {
+
+// When user downloads a course on the app it will update their user document
+// to contain the course id and completion. It will also contain all the connected
+// sections and exercises to that id and their completion status.
+// If the user tries to download it again, it should return the course, section, 
+// and exercises and their completion status so they can continue where they left off.
+router.post('/:id/enroll/:courseId', async (req, res, next) => {
   try {
-    // find the user by ID
-    const user = await appUserModel.findById(req.params.id);
+    const { id, courseId } = req.params;
+    const user = await appUserList.findById(id);
     if (!user) {
       return res.status(404).json({
         message: 'User not found'
@@ -212,7 +188,7 @@ router.post('/:id/enroll/:courseId', async function(req, res, next) {
     const existingCourse = await findCourseById(user, req.params.courseId);
 
     // find the course by ID and populate the sections
-    const course = await CourseModel.findById(req.params.courseId).populate('sections');
+    const course = await CourseModel.findById(courseId).populate('sections');
     if (!course) {
       return res.status(404).json({
         message: 'Course not found'
@@ -220,7 +196,7 @@ router.post('/:id/enroll/:courseId', async function(req, res, next) {
     }
 
     // create a new course object
-    let newCourse = {
+    const newCourse = {
       courseId: course.id, 
       isComplete: false, 
       sections: []
@@ -228,17 +204,17 @@ router.post('/:id/enroll/:courseId', async function(req, res, next) {
 
     // loop through each section in the course
     for (const section of course.sections) {
-      // find the section object from the SectionModel
       const foundSection = await SectionModel.findById(section.id).populate('exercises');
 
-      // create a new section object that includes the ID and exercises for the section, and add it to the sections array in the newCourse object
-      let newSection = {
+      // create a new section object
+      const newSection = {
         section: foundSection.id,
         isComplete: false,
         exercises: [] 
       };
 
-      // loop through each exercise in the section and add it to the exercises array in the newSection object
+      // loop through each exercise in the section and add it to the exercises 
+      // array in the newSection object
       for (const exercise of foundSection.exercises) {
         newSection.exercises.push({
           exercise: exercise.id,
@@ -249,46 +225,44 @@ router.post('/:id/enroll/:courseId', async function(req, res, next) {
       newCourse.sections.push(newSection);
     }
 
-// if the course does not already exist in the user's activeCourses list, push the new course object to the activeCourses array in the user object
-if (!existingCourse) {
-  user.activeCourses.push(newCourse);
-} else {
-  // if the course already exists, update the existing course object with the new information
-  existingCourse.sections = newCourse.sections;
-}
+    if (!existingCourse) {
+      user.activeCourses.push(newCourse);
+    } else {
+      // if the course already exists, update the existing course object with the new information
+      existingCourse.sections = newCourse.sections;
+    }
 
-// save the updated user object to the database
-await user.save();
+    // save the updated user object to the database
+    await user.save();
 
-// return the updated user object in the response
 if (!existingCourse) {
-  // if the course does not already exist, return the updated user object with the new course information
-  res.json(user);
+    return res.json(user);
 } else {
-  // if the course already exists, return the existing course information
-  res.json({
-    course: {
-    id: existingCourse.courseId,
-    isComplete: existingCourse.isComplete,
-    sections: existingCourse.sections.map(section => {
-      return {
-        id: section.section,
-        isComplete: section.isComplete,
-        exercises: section.exercises.map(exercise => {
-          return {
-            id: exercise.exercise,
-            isComplete: exercise.isComplete
-          };
-        })
-      };
-    })
-  }
-  });
+    // if the course already exists, return the existing course information
+    return res.json({
+        course: {
+            id: existingCourse.courseId,
+            isComplete: existingCourse.isComplete,
+            sections: existingCourse.sections.map(section => {
+                return {
+                    id: section.section,
+                    isComplete: section.isComplete,
+                    exercises: section.exercises.map(exercise => {
+                        return {
+                            id: exercise.exercise,
+                            isComplete: exercise.isComplete
+                        };
+                    })
+                };
+            })
+        }
+    });
 }
   } catch (err) {
     return next(err);
   }
 });
+
 
 // find the course in the user's activeCourses list
 async function findCourseById(user, courseId) {
@@ -301,6 +275,7 @@ async function findCourseById(user, courseId) {
   return null;
 }
 
+
 // find the section in the course's sections array
 async function findSectionById(course, sectionId) {
   if (!course) return null;
@@ -312,6 +287,7 @@ async function findSectionById(course, sectionId) {
   }
   return null;
 }
+
 
 // find the exercise in the section's exercises array
 async function findExerciseById(section, exerciseId) {
